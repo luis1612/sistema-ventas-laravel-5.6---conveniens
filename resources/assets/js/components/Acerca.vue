ee7530cb225e6235aebdcf9b67524d25c1a8d259
luis1612 <ibarguen1294@gmail.com>
2022-01-08 16:35:17 -0500

    init commit;

---
+++ resources/assets/js/components/Acerca.vue
@@ -0,0 +1,29 @@
+<template>
+            <main class="main">
+            <!-- Breadcrumb -->
+            <ol class="breadcrumb">
+                
+            </ol>
+            <div class="container-fluid">
+                <!-- Ejemplo de tabla Listado -->
+                <div class="card">
+                    <div class="card-header">
+                        <i class="fa fa-align-justify"></i> Ayuda
+                    </div>
+                    <div class="card-body">
+                        <div class="form-group row">
+                            <div class="col-md-6">
+                                <h4>Proyecto: </h4> <p>ItVentas 1.0 - Sistema de Ventas, Compras para Almacén Conveniens Fashion Store</p>
+                                <!-- <h4>Empresa: </h4> <p>Incanatoit.com</p> -->
+                                <h4>Desarrollado por: </h4> <p>ibarguen1294@gmail.com</p>
+                                <h4>Web: </h4><a href="https://github.com/luis1612" target="_blank"> <p>https://github.com/luis1612</p></a>
+                                <h4>Telefono: </h4> <a href="" target=""><p>+57 3202545841</p></a>
+                                <!-- <h4>Canal Youtube: </h4> <a href="https://www.youtube.com/jcarlosad7" target="_blank"><p>www.youtube.com/jcarlosad</p></a> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Fin ejemplo de tabla Listado -->
+            </div>
+        </main>
+</template>
